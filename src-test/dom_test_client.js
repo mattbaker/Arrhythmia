c0a7b2824e73501499ccc1a07ca38bc2a20dd0c1
Matt Baker <mbaker.pdx@gmail.com>
2010-11-22 09:42:21 -0800

    Added README, DOM tests stand alone as examples

--- src-test/dom_test_client.js
+++ src-test/dom_test_client.js
@@ -1,5 +1,5 @@
 $(function() {
-	if(window.opener) {
+	if(window.opener && window.opener.DOMTest) {
 		window.DOMTestClient = {};
 	    window.DOMTestClient.execute = function(fnc) {
 		    window.opener.DOMTest.clientFinished(fnc.apply(window));
